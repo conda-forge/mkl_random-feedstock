3cec1301cda1abc96f403c6ba03a08774fdd798f
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2020-09-17 08:59:16 -0500

    fix build.sh by using

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 cp $RECIPE_DIR/mkl_beta_nan.cpp .
-gcc -c -std=c++17 -I$PREFIX/include mkl_beta_nan.cpp -o mkl_beta_nan.o
-gcc -L$PREFIX/lib -lmkl_rt -lstdc++ -Wl,-rpath,$CONDA_PREFIX/lib mkl_beta_nan.o -o mkl_beta_nan.exe
+$GXX -c -std=c++17 -I$PREFIX/include mkl_beta_nan.cpp -o mkl_beta_nan.o
+$GXX -L$PREFIX/lib -lmkl_rt -lstdc++ -Wl,-rpath,$CONDA_PREFIX/lib mkl_beta_nan.o -o mkl_beta_nan.exe
 
 ./mkl_beta_nan.exe
 
