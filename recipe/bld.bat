2c4cc8c432024c237319239ba5ebeb29cca7f90a
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2020-09-17 08:39:19 -0500

    fixed bld.bat, also added compilation of the reproducer to build.sh

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
-copy /y %RECIPE_DIR\mkl_beta_nan.cpp .
+copy /y %RECIPE_DIR%\mkl_beta_nan.cpp .
 
 cl.exe /c /nologo /Ox /W3 -I"%PREFIX%/Library/include" mkl_beta_nan.cpp /Fomkl_beta_nan.obj
 link.exe /LIBPATH:"%PREFIX%/Library/lib" mkl_rt.lib mkl_beta_nan.obj  /OUT:mkl_beta_nan.exe
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -x
+cp $RECIPE_DIR/mkl_beta_nan.cpp .
+gcc -c -I$PREFIX/include mkl_beta_nan.cpp -o mkl_beta_nan.o
+gcc -L$PREFIX/lib -lmkl_rt mkl_beta_nan.o -o mkl_beta_nan.exe
+
+./mkl_beta_nan.exe
+
 $PYTHON setup.py config build install --old-and-unmanageable
 
