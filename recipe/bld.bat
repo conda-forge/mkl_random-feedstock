635e699d5210e4a743d00b9cce441dbae77a1336
Harlow, Jordan <jordan.harlow@intel.com>
2026-08-21 08:08:58 -0600

    Migrate recipe to meson-python for 1.5.0

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,6 @@
-set MKLROOT=%PREFIX%
-%PYTHON% -m pip install --no-build-isolation --no-deps .
+:: MKL is found through %LIBRARY_PREFIX%\lib\cmake\mkl, which is on
+:: CMAKE_PREFIX_PATH via the vs2022 activation script. Setting MKLROOT here
+:: would break the build: MKLConfig.cmake looks for %MKLROOT%\include with
+:: NO_DEFAULT_PATH and %PREFIX%\include holds no MKL headers.
+%PYTHON% -m pip install . -vv --no-build-isolation --no-deps
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,7 @@
-#!/bin/bash -x
-export MKLROOT=${PREFIX}
-$PYTHON -m pip install --no-build-isolation --no-deps .
+#!/bin/bash -ex
+
+# meson locates MKL via find_package(MKL CONFIG) -> $PREFIX/lib/cmake/mkl,
+# which is on CMAKE_PREFIX_PATH thanks to the compiler activation scripts.
+# Do not export MKLROOT: MKLConfig.cmake then searches $MKLROOT/include with
+# NO_DEFAULT_PATH and hard-errors if mkl_version.h is not there.
+$PYTHON -m pip install . -vv --no-build-isolation --no-deps
